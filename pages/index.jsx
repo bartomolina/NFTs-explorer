de892dece1881bb5c4cddd1d68255ec57bda372c
Barto Molina <bartomolina@gmail.com>
2022-10-21 17:21:25 +0200

    Fixed collection text field properties

--- pages/index.jsx
+++ pages/index.jsx
@@ -65,7 +65,8 @@ const Home = () => {
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input disabled={fetchForCollection} className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-000 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e) => { setWalletAddress(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address"></input>
-        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-000 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e) => { setWalletAddress(e.target.value) }} value={wallet} type={"text"} placeholder="Add your wallet address" onChange={(e) => { setCollectionAddress(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address"></input>
+        <input className="w-2/5 bg-slate-100 py-2 px-2 rounded-lg text-gray-000 focus:outline-blue-300 disabled:bg-slate-50 disabled:text-gray-50" onChange={(e) => { setCollectionAddress(e.target.value) }} value={collection} type={"text"} placeholder="Add the collection address">
+        </input>
         <label className="text-gray-600">
           <input checked={fetchForCollection} className="mr-2" onChange={(e) => { setFetchForCollection(e.target.checked) }} type={"checkbox"}></input>
           Fetch for collection
